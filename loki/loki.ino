9d15cfbf3f76d1358b177552ca7fa0cb91b021e3
david <david.sanchez.falero@gmail.com>
2015-09-23 17:31:48 +0200

    new loki code

--- loki/loki.ino
+++ loki/loki.ino
@@ -18,103 +18,99 @@
  */
 
 
-int speed3Pin = 3;
-int direction3Pin = 2;
-int speed1Pin = 5;
-int direction1Pin = 4;
+int speed1Pin = 3;
+int direction1Pin = 2;
+int speed0Pin = 5;
+int direction0Pin = 4;
 int speed2Pin = 6;
 int direction2Pin = 7;
 
+float arms_size = 110.74;
+
+float speed_0 = 0;
+float speed_1 = 0;
+float speed_2 = 0;
+float speed_X = 0;
+float speed_Y = 0;
+float speed_W = 0;
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+boolean startMessage = false;
+
 void setup() {
-
   Serial.begin(19200);
-
 }
 
 void loop() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
-    //Serial.println(inputString);
-    if (inChar == 'r') {
-      right();
+
+    if (inChar == ')') {
+      startMessage = false;
+      stringComplete = true;
+      inputString += ';';
+      Serial.flush();
     }
-    if (inChar == 'l') {
-      left();
+    if (startMessage) inputString += inChar;
+    if (inChar == '(' && !startMessage) startMessage = true;
+    if (inChar == '(' && startMessage) inputString = "";
+    if (stringComplete) break;
+  }
+  if (stringComplete) {
+    int message_substring = 0;
+    String substr = "";
+    for (int i = 0 ; i < inputString.length(); i++) {
+      if (inputString[i] == ';') {
+        message_substring++;
+        switch (message_substring) {
+          case 1:
+            speed_X = substr.toFloat();
+            substr = "";
+          case 2:
+            speed_Y = substr.toFloat();
+            substr = "";
+          case 3:
+            speed_W = substr.toFloat();
+            substr = "";
+        }
+      }
+      else substr += (char)inputString[i];
     }
-    if (inChar == 'f') {
-      front();
-    }
-    if (inChar == 'b') {
-      back();
-    }
-    if (inChar == 'c') {
-      clockwise();
-    }
-    if (inChar == 'a') {
-      counterclockwise();
-    }
-    if (inChar == 's') {
-      stop_all();
-    }
-    Serial.flush();
+    inputString = "";
+    stringComplete = false;
+    combine_movements();
   }
 }
 
-void front() {
-  digitalWrite(direction1Pin, 0);
-  analogWrite(speed1Pin, 255);
-  digitalWrite(direction2Pin, 1);
-  analogWrite(speed2Pin, 255);
-  analogWrite(speed3Pin, 0);
-}
+void combine_movements() {
 
-void back() {
-  digitalWrite(direction1Pin, 1);
-  analogWrite(speed1Pin, 255);
-  digitalWrite(direction2Pin, 0);
-  analogWrite(speed2Pin, 255);
-  analogWrite(speed3Pin, 0);
-}
+  speed_0 = (-speed_X / sin(PI / 3)) + (speed_Y / cos(PI / 3)) + (arms_size * speed_W);
+  speed_1 = (-speed_Y + arms_size * speed_W);
+  speed_2 = (speed_X / sin(PI / 3)) + (speed_Y / cos(PI / 3)) + (arms_size * speed_W);
+  float norm = 1.0;
+  if (abs(speed_0) > 1 || abs(speed_1) > 1 || abs(speed_2) > 1) {
+    if (abs(speed_0) >= abs(speed_1)) {
+      norm = abs(speed_0);
+      if (abs(speed_2) > abs(speed_0)) norm = abs(speed_2);
 
-void stop_all() {
-  analogWrite(speed1Pin, 0);
-  analogWrite(speed2Pin, 0);
-  analogWrite(speed3Pin, 0);
-}
+    }
+    else {
+      norm = abs(speed_1);
+      if (abs(speed_2) > abs(speed_1)) norm = abs(speed_2);
+    }
+  }
 
-
-void left() {
-  digitalWrite(direction1Pin, 1);
-  analogWrite(speed1Pin, 125);
-  digitalWrite(direction2Pin, 1);
-  analogWrite(speed2Pin, 125);
-  digitalWrite(direction3Pin, 0);
-  analogWrite(speed3Pin, 255);
-}
-
-void right() {
-  digitalWrite(direction1Pin, 0);
-  analogWrite(speed1Pin, 125);
-  digitalWrite(direction2Pin, 0);
-  analogWrite(speed2Pin, 125);
-  digitalWrite(direction3Pin, 1);
-  analogWrite(speed3Pin, 255);
-}
-
-void clockwise() {
-  digitalWrite(direction1Pin, 0);
-  analogWrite(speed1Pin, 150);
-  digitalWrite(direction2Pin, 0);
-  analogWrite(speed2Pin, 150);
-  digitalWrite(direction3Pin, 0);
-  analogWrite(speed3Pin, 150);
+  speed_0 = map(abs(speed_0/norm), 0, 1.0, 0, 255);
+  speed_1 = map(abs(speed_0/norm), 0, 1.0, 0, 255);
+  speed_2 = map(abs(speed_0/norm), 0, 1.0, 0, 255);
 }
 
 void counterclockwise() {
-  digitalWrite(direction1Pin, 1);
-  analogWrite(speed1Pin, 150);
+  digitalWrite(direction0Pin, 1);
+  analogWrite(speed0Pin, 150);
   digitalWrite(direction2Pin, 1);
   analogWrite(speed2Pin, 150);
-  digitalWrite(direction3Pin, 1);
-  analogWrite(speed3Pin, 150);
+  digitalWrite(direction1Pin, 1);
+  analogWrite(speed1Pin, 150);
 }
