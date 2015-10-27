860c34d5dea271ed9b7ee354c1fa761b0e1f88cc
David <david.sanchez.falero@gmail.com>
2015-10-27 16:01:51 +0100

    server updated

--- pathfinder/server.py
+++ pathfinder/server.py
@@ -1,14 +1,14 @@
 import socket
 import json
 from pathfinder import Pathfinder
-
+import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("172.16.17.26", 8090))
 server.listen(1)
 robot = Pathfinder()
 client, addr = server.accept()
-
+client.settimeout(5)
 while True:
     msg = client.recv(1024)
     if msg == "quit":
