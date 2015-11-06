b8f74f0867110cbe5a5ee1bad465e782f96c7cf4
David <david.sanchez.falero@gmail.com>
2015-11-06 16:50:53 +0100

    server updated

--- pathfinder/python/pathfinder/server/server.py
+++ pathfinder/python/pathfinder/server/server.py
@@ -1,5 +1,7 @@
 import socket
 import json
+import psutil
+import subprocess
 try:
     from pathfinder.robot.pathfinder import Pathfinder
 except:
@@ -18,29 +20,40 @@ def robot_server():
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((TCP_IP, TCP_ROBOT_PORT))
+    subp = subprocess.Popen(['./mjpg_streamer', '-i', './input_uvc.so -f 15 -r 640x480 -n', '-o' ,'./output_http.so -w ./www'],
+                            cwd='/root/mjpg-streamer',
+                            shell=False,
+                            stdin=None,
+                            stdout=None,
+                            stderr=None,
+                            close_fds=True)
+    streaming = psutil.Process(subp.pid)
     while True:
         server.listen(1)
         robot = Pathfinder()
         robot.move(0, 0, 0)
         client, _ = server.accept()
         client.settimeout(2)
+        print "MOVING ..."
         try:
             while True:
                 msg = client.recv(1024)
-            if msg == "quit":
-                break
-            else:
-                try:
-                    msg = json.loads(msg)
-                    robot.move(msg['x'], msg['y'], msg['w'],)
-                except ValueError:
-                    pass
+                if msg == "quit":
+                    break
+                else:
+                    try:
+                        msg = json.loads(msg)
+                        robot.move(msg['x'], msg['y'], msg['w'],)
+                    except ValueError:
+                        pass
         except socket.timeout:
             print "robot_server, socket.timeout"
         except socket.error:
             print "robot_server, socket.error"
+        except KeyboardInterrupt:
+            pass
         robot.move(0, 0, 0)
-        client.close()
+    streaming.kill()
     server.close()
 
 if __name__ == '__main__':
