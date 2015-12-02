8750cf15320e95b42f25d0b96bc3751cbfe52ef3
david <david.sanchez.falero@gmail.com>
2015-12-02 18:36:14 +0100

    threading app added

--- mini_loki/python/loki/mini_loki/client.py
+++ mini_loki/python/loki/mini_loki/client.py
@@ -1,14 +1,20 @@
 import socket
 import pygame
+import time
 
 
-def client(ip="loki.local", port=80):
+def client(ip="loki_0.local", port=80, robots=None, close_event=None):
 
+    while len(robots) < 1:
+        if close_event.is_set():
+            return
+        time.sleep(0.1)
     pygame.init()
     pygame.joystick.init()
     joystick = pygame.joystick.Joystick(0)
     joystick.init()
     s = socket.socket()
+    time.sleep(1)
     s.connect((ip, port))
     try:
         while True:
@@ -19,7 +25,13 @@ def client(ip="loki.local", port=80):
             y = float(joystick.get_axis(0))
             x = float(joystick.get_axis(1))
             w = float(joystick.get_axis(2))
+            if joystick.get_button(6) and not joystick.get_button(7):
+                x, y, w = 0.00, -1.00, 0.50
+            if joystick.get_button(7) and not joystick.get_button(6):
+                x, y, w = 0.00, 1.00, -0.50
             s.send("%.2f,%.2f,%.2f,\n" % (x, y, w))
+            if close_event.is_set():
+                break
     except socket.error:
         pass
     s.close()
--- mini_loki/python/loki/mini_loki/overmind.py
+++ mini_loki/python/loki/mini_loki/overmind.py
@@ -1,39 +1,44 @@
-import socket
-from mini_loki.client import client
-import json
 from _collections import defaultdict
-from time import sleep
+import time
+import threading
+from mini_loki.swarm_organizer import organizer_server
+from mini_loki.client import client
+
 
 robots = defaultdict()
 close_all = False
 
 
-def server(ip="172.16.17.52", port=5044):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((ip, port))
-    server.listen(1)
-    client, _ = server.accept()
-    client.settimeout(2)
-    robot_dns = None
+class MiThread(threading.Thread):
+    def __init__(self, service,
+                 close_event=None,
+                 ip="172.16.17.136",
+                 port=5044):
+        threading.Thread.__init__(self)
+        self.close_event = close_event
+        self.service = service
+        self.ip = ip
+        self.port = port
+
+    def run(self):
+        self.service(self.ip, self.port, robots, self.close_event)
+
+
+def overmind(ip):
+    close_all = threading.Event()
+    organizer_thread = MiThread(organizer_server, close_all, ip)
+    client_thread = MiThread(client, close_all, "loki_0.local", 80)
+    organizer_thread.start()
+    client_thread.start()
     try:
-        msg = client.recv(1024)
-        print msg
-        robot_dns = "loki_%s" % len(robots)
-        robots[robot_dns] = json.loads(msg)
-        print robots
-        client.send("%s\n" % robot_dns)
-    except socket.timeout:
-        pass
-    except socket.error:
-        pass
+        while True:
+            time.sleep(0.1)
     except KeyboardInterrupt:
-        close_all = True
-    server.close()
-    return robot_dns
+        close_all.set()
+        organizer_thread.join()
+        client_thread.join()
+
 
 if __name__ == '__main__':
-    while not close_all:
-        robot_dns = server()
-        sleep(1)
-        if robot_dns:
-            client("%s.local" % robot_dns)
+    overmind("172.16.17.57")
+    print robots
